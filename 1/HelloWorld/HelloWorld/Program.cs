07e4e818e20cc962d060bcf35531badb0a4a95a2
smasher <iluha_m_74@mail.ru>
2022-09-24 01:44:58 +0500

    Test

--- 1/HelloWorld/HelloWorld/Program.cs
+++ 1/HelloWorld/HelloWorld/Program.cs
@@ -4,6 +4,7 @@ namespace Slide01
 {
     class Program
     {
+<<<<<<< Updated upstream
         public static int SelfReverse(int numb){
             var numbCharArray = numb.ToString().ToCharArray();
             Array.Reverse(numbCharArray);
@@ -19,6 +20,35 @@ namespace Slide01
         public static void Main()
         {
             
+=======
+
+        static bool ShouldFire(bool enemyInFront, string enemyName, int robotHealth)
+        {
+            bool shouldFire = true;
+            if (enemyInFront == true)
+            {
+                if (enemyName == "boss")
+                {
+                    if (robotHealth < 50) shouldFire = false;
+                    if (robotHealth > 100) shouldFire = true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return shouldFire;
+        }
+
+        static bool ShouldFire2(bool enemyInFront, string enemyName, int robotHealth)
+        {
+            return enemyInFront && !(enemyName == "boss" && robotHealth < 50);
+        }
+
+
+        public static void Main()
+        {
+>>>>>>> Stashed changes
         }
 
     }
