f5827db1fe664f7c2ce8f78e07c9d43697bb11b3
smasher <iluha_m_74@mail.ru>
2022-11-05 23:54:16 +0500

    SearchSort module

--- Passwords/CaseAlternatorTask.cs
+++ Passwords/CaseAlternatorTask.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Passwords
 {
@@ -13,34 +12,22 @@ namespace Passwords
             return result;
         }
 
-        public static char[] ReturnCopiedArray(char[] arr)
-        {
-            var copiedArr = new char[arr.Length];
-            Array.Copy(arr, copiedArr, arr.Length);
-            return copiedArr;
-        }
-
         static void AlternateCharCases(char[] word, int startIndex, List<string> result)
         {
             if (startIndex == word.Length)
             {
-                var finalWord = new string(ReturnCopiedArray(word));
+                var finalWord = new string(word);
                 if (!result.Contains(finalWord))
                     result.Add(finalWord);
                 return;
             }
 
-            for (var i = 0; i < 2; i++)
+            word[startIndex] = char.ToLower(word[startIndex]);
+            AlternateCharCases(word, startIndex + 1, result);
+            if (char.IsLetter(word[startIndex]))
             {
-                if (char.IsLetter(word[startIndex]))
-                {
-                    word[startIndex] = (i == 0) ? char.ToLower(word[startIndex]) : char.ToUpper(word[startIndex]);
-                    AlternateCharCases(word, startIndex + 1, result);
-                }
-                else if (i == 0)
-                    continue; // skip one cycle for not letter
-                else
-                    AlternateCharCases(word, startIndex + 1, result);
+                word[startIndex] = char.ToUpper(word[startIndex]);
+                AlternateCharCases(word, startIndex + 1, result);
             }
         }
     }
---
+++ SearchSort/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace SearchSort
+{
+    internal class Program
+    {
+        public static void BubbleSortRange(int[] array, int left, int right)
+        {
+            for (int i = left; i <= right; i++)
+                for (int j = i + 1; j <= right - 1; j++)
+                    if (array[j] > array[j + 1])
+                    {
+                        var t = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = t;
+                    }
+        }
+
+        public static void MyBubbleSortRange(int[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+                for (var j = i + 1; j < array.Length - 1; j++)
+                    if (array[j] > array[j + 1])
+                        Swap(array, j, j + 1);
+        }
+
+        public static void Swap(int[] arr, int indexOne, int indexTwo)
+        {
+            var temp = arr[indexTwo];
+            arr[indexTwo] = arr[indexOne];
+            arr[indexOne] = temp;
+        }
+
+        public static void Print(int[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            var ints = new int[] { 1, 5, 3, 7, 9, 5, 4, 3, 1, 2 };
+            MyBubbleSortRange(ints);
+            Print(ints);
+        }
+    }
+}
