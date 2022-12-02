43e66432816daa2f820db7ee9895de36534439d3
smasher <iluha_m_74@mail.ru>
2022-12-03 02:57:39 +0500

    Made Nasledovanie module

--- ObjectModule/Program.cs
+++ ObjectModule/Program.cs
@@ -5,18 +5,61 @@ using System;
 
 namespace ObjectModule
 {
+    public class Point
+    {
+        public double X;
+        public double Y;
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", X, Y);
+        }
+    }
+
+    class Triangle
+    {
+        public Point A;
+        public Point B;
+        public Point C;
+
+        public override string ToString()
+        {
+            return string.Format("({0}) ({1}) ({2})", A.ToString(), B.ToString(), C.ToString());
+        }
+    }
+
+    class Book : IComparable
+    {
+        public string Title;
+        public int Theme;
+
+        public int CompareTo(object? obj)
+        {
+            var bookObject = obj as Book;
+
+            if (Theme > bookObject.Theme)
+                return 1;
+            else if (Theme < bookObject.Theme)
+                return -1;
+            else if (string.CompareOrdinal(Title, bookObject.Title) < 0)
+                return -1;
+            else if (string.CompareOrdinal(Title, bookObject.Title) > 0)
+                return 1;
+            
+            return 0;
+        }
+    }
+
     class Program
     {
-        public static void Main()
+        static void Main()
         {
-            var array1 = new[] { 1, 2, 3 };
-            var array2 = array1;
-            array2[0] += 10;
-            array1[1] += 1324;
-            Console.WriteLine(array1[0]);
-            Console.WriteLine(array2[0]);
-            Console.WriteLine(array1[1]);
-            Console.WriteLine(array2[1]);
+            var triangle = new Triangle
+            {
+                A = new Point { X = 0, Y = 0 },
+                B = new Point { X = 1, Y = 2 },
+                C = new Point { X = 3, Y = 2 }
+            };
+            Console.WriteLine(triangle.ToString());
         }
     }
 }
