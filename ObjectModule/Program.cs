d7df3757a7ae591a1b58bf94870c950666e2badc
smasher <iluha_m_74@mail.ru>
2022-12-02 18:47:25 +0500

    new

--- ObjectModule/Program.cs
+++ ObjectModule/Program.cs
@@ -7,65 +7,16 @@ namespace ObjectModule
 {
     class Program
     {
-        public static Array? Combine(params Array[] arguments)
-        {
-            var combinedArrayType = CheckCombinedArrayType(arguments);
-            if (combinedArrayType == null)
-                return null;
-
-            var combinedArrayLength = 0;
-            foreach (var arg in arguments)
-                combinedArrayLength += arg.Length;
-            var combinedArray = Array.CreateInstance(combinedArrayType, combinedArrayLength);
-
-            var counter = 0;
-            for (var i = 0; i < arguments.Length; i++)
-                foreach (var elem in arguments[i])
-                    combinedArray.SetValue(elem, counter++);
-
-
-            return combinedArray;
-        }
-
-        public static Type CheckCombinedArrayType(params Array[] arguments)
-        {
-            if (arguments.Length == 0)
-                return null;
-
-            var arraysType = new Type?[arguments.Length];
-            for (var i = 0; i < arguments.Length; i++)
-                arraysType[i] = arguments[i].GetType().GetElementType();
-
-            for (var i = 1; i < arguments.Length; i++)
-                if (arraysType[i - 1] != arraysType[i])
-                    return null;
-
-            return arraysType[0];
-        }
-
         public static void Main()
         {
-            var ints = new[] { 1, 2 };
-            var strings = new[] { "A", "B" };
-
-            Print(Combine(ints, ints));
-            Print(Combine(ints, ints, ints));
-            Print(Combine(ints));
-            Print(Combine());
-            Print(Combine(strings, strings));
-            Print(Combine(ints, strings));
-        }
-
-        static void Print(Array array)
-        {
-            if (array == null)
-            {
-                Console.WriteLine("null");
-                return;
-            }
-            for (int i = 0; i < array.Length; i++)
-                Console.Write("{0} ", array.GetValue(i));
-            Console.WriteLine();
+            var array1 = new[] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] += 10;
+            array1[1] += 1324;
+            Console.WriteLine(array1[0]);
+            Console.WriteLine(array2[0]);
+            Console.WriteLine(array1[1]);
+            Console.WriteLine(array2[1]);
         }
     }
 }
