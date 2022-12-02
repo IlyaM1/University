cb6c6a8ff11a9ff82675621554711c404b29b7c5
smasher <iluha_m_74@mail.ru>
2022-12-02 18:39:28 +0500

    New

--- ObjectModule/Program.cs
+++ ObjectModule/Program.cs
@@ -3,128 +3,69 @@ using System.Xml.Linq;
 
 using System;
 
-namespace GeometryTasks
-{
-    public class Vector
-    {
-        public double X;
-        public double Y;
-    }
-
-    public class Segment
-    {
-        public Vector Begin;
-        public Vector End;
-    }
-
-    public class Geometry
-    {
-        public static double GetLength(Vector vec)
-        {
-            return Math.Sqrt(vec.X * vec.X + vec.Y * vec.Y);
-        }
-
-        public static Vector Add(Vector vec1, Vector vec2)
-        {
-            var newVec = new Vector();
-            newVec.X = vec1.X + vec2.X;
-            newVec.Y = vec1.Y + vec2.Y;
-            return newVec;
-        }
-
-        public static double GetLength(Segment seg)
-        {
-            return Math.Sqrt(((seg.End.X - seg.Begin.X) * (seg.End.X - seg.Begin.X))
-                                 + ((seg.End.Y - seg.Begin.Y) * (seg.End.Y - seg.Begin.Y)));
-        }
-
-        public static bool IsVectorInSegment(Vector point, Segment seg)
-        {
-            return GetDistanceToSegment(seg, point) == 0;
-        }
-
-        public static double GetDistanceToSegment(Segment seg, Vector vec)
-        {
-            if ((seg.Begin.X == vec.X && seg.Begin.Y == vec.Y) || (seg.End.X == vec.X && seg.End.Y == vec.Y))
-                return 0;
-
-            var ab = seg;
-            var ac = new Segment() { Begin = seg.Begin, End = vec };
-            var bc = new Segment() { Begin = seg.End, End = vec };
-
-            var lengthAB = GetLength(ab);
-            var lengthAC = GetLength(ac);
-            var lengthBC = GetLength(bc);
-
-            if (lengthAB == 0)
-                return lengthAC;
-
-            if (IsObtuse(lengthAC, lengthBC, lengthAB))
-                return lengthBC;
-            if (IsObtuse(lengthBC, lengthAC, lengthAB))
-                return lengthAC;
-
-            var semiPerimeter = (lengthAC + lengthBC + lengthAB) / 2;
-            return 2 * CountHeronsFormule(semiPerimeter, lengthAB, lengthBC, lengthAC) / lengthAB;
-        }
-
-        public static bool IsObtuse(double line, double a, double b)
-        {
-            return (a * a + b * b - line * line) / (2 * a * b) < 0;
-        }
-
-        public static double CountHeronsFormule(double p, double a, double b, double c)
-        {
-            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
-        }
-    }
-
-    public static class VectorExtensions
-    {
-        public static double GetLength(this Vector vec)
-        {
-            return Geometry.GetLength(vec);
-        }
-
-        public static Vector Add(this Vector vec1, Vector vec2)
-        {
-            return Geometry.Add(vec1, vec2);
-        }
-
-        public static bool Belongs(this Vector point, Segment seg)
-        {
-            return Geometry.IsVectorInSegment(point, seg);
-        }
-    }
-
-    public static class SegmentExtensions
-    {
-        public static double GetLength(this Segment seg)
-        {
-            return Geometry.GetLength(seg);
-        }
-
-        public static bool Contains(this Segment seg, Vector point)
-        {
-            return Geometry.IsVectorInSegment(point, seg);
-        }
-    }
-}
-
 namespace ObjectModule
 {
-    using GeometryTasks;
-
     class Program
     {
+        public static Array? Combine(params Array[] arguments)
+        {
+            var combinedArrayType = CheckCombinedArrayType(arguments);
+            if (combinedArrayType == null)
+                return null;
+
+            var combinedArrayLength = 0;
+            foreach (var arg in arguments)
+                combinedArrayLength += arg.Length;
+            var combinedArray = Array.CreateInstance(combinedArrayType, combinedArrayLength);
+
+            var counter = 0;
+            for (var i = 0; i < arguments.Length; i++)
+                foreach (var elem in arguments[i])
+                    combinedArray.SetValue(elem, counter++);
+
+
+            return combinedArray;
+        }
+
+        public static Type CheckCombinedArrayType(params Array[] arguments)
+        {
+            if (arguments.Length == 0)
+                return null;
+
+            var arraysType = new Type?[arguments.Length];
+            for (var i = 0; i < arguments.Length; i++)
+                arraysType[i] = arguments[i].GetType().GetElementType();
+
+            for (var i = 1; i < arguments.Length; i++)
+                if (arraysType[i - 1] != arraysType[i])
+                    return null;
+
+            return arraysType[0];
+        }
+
         public static void Main()
         {
-            Vector vector = new Vector() { X = 5, Y = 6 };
-            Vector vector2 = new Vector() { X = 3, Y = 4 };
-            Segment segment = new Segment() { Begin = vector, End = vector2 };
-            var test_result = vector.Belongs(segment);
-            Console.WriteLine(test_result);
-            Console.WriteLine(new Vector().GetLength());
+            var ints = new[] { 1, 2 };
+            var strings = new[] { "A", "B" };
+
+            Print(Combine(ints, ints));
+            Print(Combine(ints, ints, ints));
+            Print(Combine(ints));
+            Print(Combine());
+            Print(Combine(strings, strings));
+            Print(Combine(ints, strings));
+        }
+
+        static void Print(Array array)
+        {
+            if (array == null)
+            {
+                Console.WriteLine("null");
+                return;
+            }
+            for (int i = 0; i < array.Length; i++)
+                Console.Write("{0} ", array.GetValue(i));
+            Console.WriteLine();
         }
     }
 }
