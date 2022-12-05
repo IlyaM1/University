6bc0c285d5e7c438ba0ff7f2b867eb14a4769e37
smasher <iluha_m_74@mail.ru>
2022-12-05 21:46:51 +0500

    ы

--- PocketGoogle/Indexer.cs
+++ PocketGoogle/Indexer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,31 @@ namespace PocketGoogle
 {
     public class Indexer : IIndexer
     {
+        private readonly char[] _separators = new char[] { ' ', '.', ',', '!', '?', ':', '-', '\r', '\n' };
+        public Dictionary<int, string> TextWordsID { get; set; }
+        private readonly Dictionary<int, Dictionary<string, List<int>>> wordsAndPosByIndex = new Dictionary<int, Dictionary<string, List<int>>>();
+        private readonly Dictionary<string, HashSet<int>> indexesByWord = new Dictionary<string, HashSet<int>>();
+
+
         public void Add(int id, string documentText)
         {
-            throw new NotImplementedException();
+            var words = documentText.Split(_separators);
+            for (int i = 0; i < words.Length; i++)
+                TextWordsID.Add(i, words[i]);
         }
 
         public List<int> GetIds(string word)
         {
-            throw new NotImplementedException();
+            var result = new List<int>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                var documentText = File.ReadAllText(String.Format("Texts/{0}.txt", i));
+                if (documentText.Split(_separators).Contains(word))
+                    result.Add(i);
+            }
+
+            return result;
         }
 
         public List<int> GetPositions(int id, string word)
