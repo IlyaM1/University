11f1e29516c83006dbc084999649eab08769075b
smasher <iluha_m_74@mail.ru>
2022-12-06 00:56:03 +0500

    ы

--- HotelAccounting/AccountingModel.cs
+++ HotelAccounting/AccountingModel.cs
@@ -1,10 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HotelAccounting
 {
-    //создайте класс AccountingModel здесь
+    public class AccountingModel : ModelBase
+    {
+        private double _price;
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                if (value < 0) 
+                    throw new ArgumentException();
+
+                _price = value;
+
+                Notify(nameof(Price));
+                Notify(nameof(Total));
+            }
+        }
+
+        private int _nightsCount;
+        public int NightsCount
+        {
+            get => _nightsCount;
+            set
+            {
+                if (value <= 0) 
+                    throw new ArgumentException();
+
+                _nightsCount = value;
+
+                Notify(nameof(NightsCount));
+                Notify(nameof(Total));
+            }
+        }
+
+        private double _discount;
+        public double Discount
+        {
+            get => _discount;
+            set
+            {
+                _discount = value;
+                _total = _price * _nightsCount * (1 - _discount / 100);
+
+                if (_total < 0) 
+                    throw new ArgumentException();
+
+                Notify(nameof(Discount));
+                Notify(nameof(Total));
+            }
+        }
+
+        private double _total;
+        public double Total
+        {
+            get => _price * _nightsCount * (1 - _discount / 100);
+            set
+            {
+                if (value <= 0) 
+                    throw new ArgumentException();
+
+                _total = value;
+                _discount = 100 * (1 - _total / (_price * _nightsCount));
+
+                Notify(nameof(Total));
+                Notify(nameof(Discount));
+            }
+        }
+    }
 }
